05793c7a693782caad553c8dd63264eebe0342e4
27Nizzo <afonso7557@gmail.com>
2024-01-29 19:36:03 +0000

    Listas :P

---
+++ lists.py
@@ -0,0 +1,4 @@
+names = ["Afonso", "Ana", "Luis", "Marta"]
+names[0] = "Nizzo"
+print(names[0:3]) #vai dar print aos 3 primeiros nomes de uma lista, ou seja, a notação [0:n] vai do 
+                  #elemento da posição 0 até ao n elemento da lista 
