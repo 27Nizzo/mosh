3df3ed118d324864215f9896d04ee339bee4740c
27Nizzo <afonso7557@gmail.com>
2024-02-03 19:38:44 +0000

    tuples

---
+++ tuples.py
@@ -0,0 +1,10 @@
+#tuples são como listas, utilizados para guardar sequencias de objetos, mas no caso dos tuples eles imutáveis
+#ou seja, são é possivel modifica-los depois de os criar
+
+numbers = (1,2,3,3)
+print(numbers.count(3))
+print(numbers.index(1))
+
+# em tuples podemos usar as funções pre-definidas, como:
+# -> .count(elemento): que vai contar quantas vezes este elemento ocorre na lista
+
